cddab580e6cc9a1dae0a16b33f9e309a60072672
Muhammad Irfan <mrfansi@outlook.com>
2023-09-13 01:46:12 +0700

    Update

--- src/CloudflareRequester.php
+++ src/CloudflareRequester.php
@@ -4,91 +4,91 @@ namespace Mrfansi\Cloudflare;
 
 class CloudflareRequester
 {
-  private static HttpClient\GuzzleClient $_httpClient;
+    private static HttpClient\GuzzleClient $_httpClient;
 
-  /**
-   * Send request and processing response
-   *
-   * @param string $method request method (get, post, patch, etc)
-   * @param string $url base url
-   * @param array $params user's params
-   * @param array $headers user's additional headers
-   *
-   * @throws Exceptions\ApiException
-   */
-  public function request(string $method, string $url, array $params = [], array $headers = []): array
-  {
-    [$responseBody, $responseCode, $responseHeaders]
-      = $this->_requestRaw($method, $url, $params, $headers);
+    /**
+     * Send request and processing response
+     *
+     * @param  string  $method request method (get, post, patch, etc)
+     * @param  string  $url base url
+     * @param  array  $params user's params
+     * @param  array  $headers user's additional headers
+     *
+     * @throws Exceptions\ApiException
+     */
+    public function request(string $method, string $url, array $params = [], array $headers = []): array
+    {
+        [$responseBody, $responseCode, $responseHeaders]
+          = $this->_requestRaw($method, $url, $params, $headers);
 
-    return json_decode($responseBody, true);
-  }
-
-  /**
-   * Set must-have headers
-   *
-   * @param array $headers user's headers
-   */
-  private function _setDefaultHeaders(array $headers): array
-  {
-    $defaultHeaders = [];
-    $xApiKey = Cloudflare::getApiKey();
-    $xApiEmail = Cloudflare::getApiEmail();
-
-    $defaultHeaders['Content-Type'] = 'application/json';
-    $defaultHeaders['X-Auth-Key'] = $xApiKey;
-    $defaultHeaders['X-Auth-Email'] = $xApiEmail;
-
-    return array_merge($defaultHeaders, $headers);
-  }
-
-  /**
-   * Send request from client
-   *
-   * @param string $method request method
-   * @param string $url additional url to base url
-   * @param array $params user's params
-   * @param array $headers request' headers
-   *
-   * @throws Exceptions\ApiException
-   */
-  private function _requestRaw(string $method, string $url, array $params, array $headers): array
-  {
-    $defaultHeaders = self::_setDefaultHeaders($headers);
-
-    $response = $this->_httpClient()->sendRequest(
-      $method,
-      $url,
-      $defaultHeaders,
-      $params
-    );
-
-    $responseBody = $response[0];
-    $responseCode = $response[1];
-    $responseHeaders = $response[2];
-
-    return [$responseBody, $responseCode, $responseHeaders];
-  }
-
-  /**
-   * Create HTTP Client
-   */
-  private function _httpClient(): HttpClient\GuzzleClient
-  {
-    if (!self::$_httpClient) {
-      self::$_httpClient = HttpClient\GuzzleClient::instance();
+        return json_decode($responseBody, true);
     }
 
-    return self::$_httpClient;
-  }
+    /**
+     * Set must-have headers
+     *
+     * @param  array  $headers user's headers
+     */
+    private function _setDefaultHeaders(array $headers): array
+    {
+        $defaultHeaders = [];
+        $xApiKey = Cloudflare::getApiKey();
+        $xApiEmail = Cloudflare::getApiEmail();
 
-  /**
-   * GuzzleClient Setter
-   *
-   * @param HttpClient\GuzzleClient $client client
-   */
-  public static function setHttpClient(HttpClient\GuzzleClient $client): void
-  {
-    self::$_httpClient = $client;
-  }
+        $defaultHeaders['Content-Type'] = 'application/json';
+        $defaultHeaders['X-Auth-Key'] = $xApiKey;
+        $defaultHeaders['X-Auth-Email'] = $xApiEmail;
+
+        return array_merge($defaultHeaders, $headers);
+    }
+
+    /**
+     * Send request from client
+     *
+     * @param  string  $method request method
+     * @param  string  $url additional url to base url
+     * @param  array  $params user's params
+     * @param  array  $headers request' headers
+     *
+     * @throws Exceptions\ApiException
+     */
+    private function _requestRaw(string $method, string $url, array $params, array $headers): array
+    {
+        $defaultHeaders = self::_setDefaultHeaders($headers);
+
+        $response = $this->_httpClient()->sendRequest(
+            $method,
+            $url,
+            $defaultHeaders,
+            $params
+        );
+
+        $responseBody = $response[0];
+        $responseCode = $response[1];
+        $responseHeaders = $response[2];
+
+        return [$responseBody, $responseCode, $responseHeaders];
+    }
+
+    /**
+     * Create HTTP Client
+     */
+    private function _httpClient(): HttpClient\GuzzleClient
+    {
+        if (! self::$_httpClient) {
+            self::$_httpClient = HttpClient\GuzzleClient::instance();
+        }
+
+        return self::$_httpClient;
+    }
+
+    /**
+     * GuzzleClient Setter
+     *
+     * @param  HttpClient\GuzzleClient  $client client
+     */
+    public static function setHttpClient(HttpClient\GuzzleClient $client): void
+    {
+        self::$_httpClient = $client;
+    }
 }
--- src/Operations/Retrieve.php
+++ src/Operations/Retrieve.php
@@ -4,15 +4,15 @@ namespace Mrfansi\Cloudflare\Operations;
 
 trait Retrieve
 {
-  /**
-   * Send GET request to retrieve data
-   *
-   * @param string $id ID
-   */
-  public static function retrieve(string $id, array $params = []): array
-  {
-    $url = static::classUrl() . '/' . $id;
+    /**
+     * Send GET request to retrieve data
+     *
+     * @param  string  $id ID
+     */
+    public static function retrieve(string $id, array $params = []): array
+    {
+        $url = static::classUrl().'/'.$id;
 
-    return static::_request('GET', $url, $params);
-  }
+        return static::_request('GET', $url, $params);
+    }
 }
