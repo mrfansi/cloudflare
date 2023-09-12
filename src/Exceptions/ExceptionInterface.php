ddf401d54076dbe24d42f981239e486fde954748
mrfansi <mrfansi@users.noreply.github.com>
2023-09-12 18:22:59 +0000

    Fix styling

--- src/Exceptions/ExceptionInterface.php
+++ src/Exceptions/ExceptionInterface.php
@@ -6,17 +6,16 @@ namespace Mrfansi\Cloudflare\Exceptions;
  * Interface ExceptionInterface
  *
  * @category Interface
- * @package  Mrfansi\Cloudflare\Exception
+ *
  * @author   Muhammad irfan <mrfansi@outlook.com>
  * @license  https://opensource.org/licenses/MIT MIT License
+ *
  * @link     https://developers.cloudflare.com/api/
  */
 interface ExceptionInterface extends \Throwable
 {
-  /**
-   * Get error code for the exception instance
-   *
-   * @return string
-   */
-  public function getErrorCode(): string;
+    /**
+     * Get error code for the exception instance
+     */
+    public function getErrorCode(): string;
 }
